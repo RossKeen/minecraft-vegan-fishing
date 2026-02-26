1fae6be300d7ef8842c101bf5c040c9bb6a7a184
RossKeen <rosssamuelkeen@gmail.com>
2026-02-26 18:46:40 +0000

    initial commit - use template mod generator for vegan-fishing

---
+++ src/client/java/social/rizza/veganfishing/VeganFishingClient.java
@@ -0,0 +1,10 @@
+package social.rizza.veganfishing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VeganFishingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/social/rizza/veganfishing/VeganFishingDataGenerator.java
@@ -0,0 +1,11 @@
+package social.rizza.veganfishing;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VeganFishingDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
